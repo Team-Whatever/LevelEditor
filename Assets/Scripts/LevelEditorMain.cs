7d733ee17649c89d0a92520f0a0b990108b3fc7a
hard-coded <cozyncoze@gmail.com>
2020-04-20 13:23:29 -0400

    intermediate submit

---
+++ Assets/Scripts/LevelEditorMain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditorMain : Singleton<LevelEditorMain>
+{
+    GameObject currentEditingObject;
+    public void Update()
+    {
+        if( currentEditingObject != null )
+        {
+            if( Input.GetMouseButton( 0 ) )
+            {
+
+            }
+            
+            //currentEditingObject.transform.position = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        }
+
+        //Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        
+        //Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        //Debug.Log( "mouse = " + Input.mousePosition + " world = " + worldPos + ", ray = " + ray );
+        //Debug.DrawRay( ray.origin, ray.direction * 10, Color.yellow );
+
+    }
+
+    public void BeginPrimitiveObject( GameObject primitiveObject )
+    {
+        if( primitiveObject != null )
+            currentEditingObject = Instantiate( primitiveObject );
+    }
+}
---
+++ Assets/Scripts/UI/LeftPanel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PrimitiveType
+{
+    Cube,
+    Sphere,
+    Cylinder,
+    Cone,
+    Torus
+}
+
+public class LeftPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSceneLoad()
+    {
+
+    }
+
+    public void OnSceneSave()
+    {
+
+    }
+
+    public void OnPrimitiveClicked( GameObject buttonObject )
+    {
+        GameObject primitiveObject = buttonObject.GetComponent<PrimitiveObject>().prefab;
+        LevelEditorMain.Instance.BeginPrimitiveObject( primitiveObject );
+    }
+
+    public void OnPlaneClicked()
+    {
+
+    }
+}
