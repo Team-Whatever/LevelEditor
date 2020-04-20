1ad0dc793ece7de9e704d4e55e2c89c3a2ff378c
hard-coded <cozyncoze@gmail.com>
2020-04-20 14:43:14 -0400

    support drag&drop

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    Vector3 offset;
+    float zCoord;
+
+    private void OnMouseDown()
+    {
+        zCoord = Camera.main.WorldToScreenPoint( gameObject.transform.position ).z;
+        offset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zCoord;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint( mousePoint );
+        Debug.Log( "mousew world pos = " + mouseWorldPos );
+        return mouseWorldPos;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+        Debug.Log( "transform pos = " + transform.position );
+    }
+}
--- Assets/Scripts/LevelEditorMain.cs
+++ Assets/Scripts/LevelEditorMain.cs
@@ -7,27 +7,33 @@ public class LevelEditorMain : Singleton<LevelEditorMain>
     GameObject currentEditingObject;
     public void Update()
     {
-        if( currentEditingObject != null )
-        {
-            if( Input.GetMouseButton( 0 ) )
-            {
-
-            }
-            
-            //currentEditingObject.transform.position = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-        }
-
-        //Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-        
-        //Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
-        //Debug.Log( "mouse = " + Input.mousePosition + " world = " + worldPos + ", ray = " + ray );
-        //Debug.DrawRay( ray.origin, ray.direction * 10, Color.yellow );
-
+        //if( currentEditingObject != null )
+        //{
+        //    if( Input.GetMouseButtonUp( 0 ) )
+        //    {
+        //        Destroy( currentEditingObject.GetComponent<DragObject>() );
+        //    }
+        //    currentEditingObject = null;
+        //}
+        //else
+        //{
+        //    if( Input.GetMouseButtonDown( 0 ) )
+        //    {
+        //        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        //        RaycastHit hitInfo;
+        //        if( Physics.Raycast( ray, out hitInfo ) )
+        //        {
+        //            currentEditingObject = hitInfo.transform.gameObject;
+        //            currentEditingObject.AddComponent<DragObject>();
+        //            Debug.Log( hitInfo.transform.gameObject );
+        //        }
+        //    }
+        //}
     }
 
     public void BeginPrimitiveObject( GameObject primitiveObject )
     {
         if( primitiveObject != null )
-            currentEditingObject = Instantiate( primitiveObject );
+            Instantiate( primitiveObject );
     }
 }
