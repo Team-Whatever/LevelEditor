7fb5231d4a00291b87ca14d3549fb9b2842edaeb
hard-coded <cozyncoze@gmail.com>
2020-04-20 18:10:43 -0400

    added file dialog package

--- Assets/Scripts/UI/LeftPanel.cs
+++ Assets/Scripts/UI/LeftPanel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Windows;
 using UnityEngine;
+using SFB;
+
 
 public enum PrimitiveType
 {
@@ -15,12 +18,15 @@ public class LeftPanel : MonoBehaviour
 {
     public void OnSceneLoad()
     {
-
+        var paths = StandaloneFileBrowser.OpenFilePanel( "Open File", "", "scene", false );
+        if( paths.Length > 0 )
+            Debug.Log( "load path = " + paths[0] );
     }
 
     public void OnSceneSave()
     {
-
+        var path = StandaloneFileBrowser.SaveFilePanel( "Save File", "", "noname", "scene" );
+        Debug.Log( "save path = " + path );
     }
 
     public void OnPrimitiveClicked( GameObject buttonObject )
